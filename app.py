0bead77f9218a436821114d04c856dd38616925e
why-piyush-18 <piyushdubey947@gmail.com>
2025-09-13 15:55:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,6 @@ def get_response():
 
 # ------------------ Run Flask ------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Cloud platforms provide PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)
+
